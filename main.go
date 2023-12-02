ebd0b6a2e4e3b104baf9eb827975a67e2efb9095
Jonathan <48375298+JonathanDowdell@users.noreply.github.com>
2023-12-02 12:22:51 -0800

    🔧 fix(main.go): add comments to improve code readability and maintainability ✨ feat(main.go): create a new database connection to MongoDB ✨ feat(main.go): create adapters, repositories, services, and handlers for GPS data ✨ feat(main.go): create a new Fiber app and router ✨ feat(main.go): define a route for saving GPS data ✨ feat(main.go): start the server on port 3000

--- main.go
+++ main.go
@@ -11,22 +11,26 @@ import (
 )
 
 func main() {
+	// Create a new database connection
 	db, err := database.NewGPSDatabaseConnection("mongodb://root:password@localhost:27017")
 	if err != nil {
 		panic(err)
 	}
-
 	defer db.Disconnect()
 
+	// Create adapters, repositories, services, and handlers
 	mongoDBAdapter := adapter.NewMongoAdapter(db.Collection)
 	gpsRepository := repository.NewMongoGPSDataRepository(mongoDBAdapter)
 	gpsService := service.NewGPSService(gpsRepository)
 	gpsHandler := api.NewGPSHandler(gpsService)
 
+	// Create a new Fiber app and router
 	app := fiber.New()
 	fiberRouter := api.NewFiberRouter(app)
 
+	// Define routes
 	fiberRouter.Post("/gps", gpsHandler.SaveGPSData)
 
+	// Start the server
 	app.Listen(":3000")
 }
