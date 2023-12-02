0104f6d09e287b98523cbd894c695a5a132d0bdb
Jonathan <48375298+JonathanDowdell@users.noreply.github.com>
2023-12-02 13:15:45 -0800

    📦 chore(mock): add mock implementation for GPSDataRepository interface 🧪 test(gpsservice_test.go): add unit tests for GPSServiceImpl.Save, GPSServiceImpl.FindByDeviceID, and GPSServiceImpl.FindByUserID methods

---
+++ service/gpsservice_test.go
@@ -0,0 +1,99 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"gps-data-receiver/model"
+	"gps-data-receiver/repository/mock"
+	"reflect"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+)
+
+func TestGPSServiceImpl_Save(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mock.NewMockGPSDataRepository(ctrl)
+	service := NewGPSService(mockRepo)
+
+	ctx := context.Background()
+	gpsData := &model.GPSData{ /* ... set test data ... */ }
+
+	// Success scenario
+	mockRepo.EXPECT().Save(ctx, gpsData).Return(nil)
+	err := service.Save(ctx, gpsData)
+	if err != nil {
+		t.Errorf("Save() error = %v, wantErr %v", err, nil)
+	}
+
+	// Error scenario
+	mockRepo.EXPECT().Save(ctx, gpsData).Return(errors.New("save error"))
+	err = service.Save(ctx, gpsData)
+	if err == nil {
+		t.Error("Save() expected an error, got nil")
+	}
+}
+
+func TestGPSServiceImpl_FindByDeviceID(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mock.NewMockGPSDataRepository(ctrl)
+	service := NewGPSService(mockRepo)
+
+	ctx := context.Background()
+	deviceID := "test-device"
+
+	// Mock data
+	gpsData := []*model.GPSData{ /* ... set test data ... */ }
+
+	// Success scenario
+	mockRepo.EXPECT().FindByDeviceID(ctx, deviceID).Return(gpsData, nil)
+	result, err := service.FindByDeviceID(ctx, deviceID)
+	if err != nil {
+		t.Errorf("FindByDeviceID() error = %v, wantErr %v", err, nil)
+	}
+	if !reflect.DeepEqual(result, gpsData) {
+		t.Errorf("FindByDeviceID() = %v, want %v", result, gpsData)
+	}
+
+	// Error scenario
+	mockRepo.EXPECT().FindByDeviceID(ctx, deviceID).Return(nil, errors.New("find error"))
+	_, err = service.FindByDeviceID(ctx, deviceID)
+	if err == nil {
+		t.Error("FindByDeviceID() expected an error, got nil")
+	}
+}
+
+func TestGPSServiceImpl_FindByUserID(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mock.NewMockGPSDataRepository(ctrl)
+	service := NewGPSService(mockRepo)
+
+	ctx := context.Background()
+	userID := "test-user"
+
+	// Mock data
+	gpsData := []*model.GPSData{ /* ... set test data ... */ }
+
+	// Success scenario
+	mockRepo.EXPECT().FindByUserID(ctx, userID).Return(gpsData, nil)
+	result, err := service.FindByUserID(ctx, userID)
+	if err != nil {
+		t.Errorf("FindByUserID() error = %v, wantErr %v", err, nil)
+	}
+	if !reflect.DeepEqual(result, gpsData) {
+		t.Errorf("FindByUserID() = %v, want %v", result, gpsData)
+	}
+
+	// Error scenario
+	mockRepo.EXPECT().FindByUserID(ctx, userID).Return(nil, errors.New("find error"))
+	_, err = service.FindByUserID(ctx, userID)
+	if err == nil {
+		t.Error("FindByUserID() expected an error, got nil")
+	}
+}
